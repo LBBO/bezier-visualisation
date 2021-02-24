708c0745442537f74b7b9d60305930bd00096836
Michael Kuckuk <mdk@kuckuk.com>
2021-02-24 15:28:01 +0100

    Add class for Plotting R -> [0,1] functions

---
+++ src/Plot.ts
@@ -0,0 +1,82 @@
+import paper from 'paper'
+import randomColor from 'randomcolor'
+
+export class Plot extends paper.Group {
+  #minX: number
+  #maxX: number
+  #stepSize: number
+  #funcs: Array<(x: number) => number>
+
+  constructor(
+    minX: number,
+    maxX: number,
+    stepSize: number,
+    funcs: Array<(x: number) => number>,
+  ) {
+    super()
+    this.#minX = minX
+    this.#maxX = maxX
+    this.#stepSize = stepSize
+    this.#funcs = funcs
+
+    this.#draw()
+  }
+
+  #draw = () => {
+    this.removeChildren()
+    this.#drawGraphs()
+    this.#drawCoordinateSystem()
+  }
+
+  #drawCoordinateSystem = () => {
+    const coordinateSystem = new paper.Group()
+    coordinateSystem.addChild(
+      new paper.Path.Line(
+        new paper.Point(0, 0),
+        new paper.Point(this.#maxX, 0),
+      ),
+    )
+    coordinateSystem.addChild(
+      new paper.Path.Line(new paper.Point(0, -0.1), new paper.Point(0, 1.1)),
+    )
+
+    coordinateSystem.addChildren(
+      Array(this.#maxX)
+        .fill(1)
+        .map(
+          (_, index) =>
+            new paper.Path.Line(
+              new paper.Point(index + 1, -0.1),
+              new paper.Point(index + 1, 0.1),
+            ),
+        ),
+    )
+
+    coordinateSystem.strokeColor = new paper.Color('grey')
+    this.addChild(coordinateSystem)
+  }
+
+  #drawGraphs = () => {
+    const colors = randomColor({
+      count: 10,
+      seed: 'abcd',
+    }).map((hexString) => new paper.Color(hexString))
+
+    this.#funcs.forEach((func, funcIndex) => {
+      const xValues = Array((this.#maxX - this.#minX) / this.#stepSize)
+        .fill(1)
+        .map((_, index) => this.#minX + index * this.#stepSize)
+
+      const points = xValues
+        .map((x) => new paper.Point(x, func(x)))
+        .filter((p) => !isNaN(p.x) && !isNaN(p.y))
+
+      const graph = new paper.Path(points)
+      graph.closed = false
+      graph.strokeColor = colors[funcIndex]
+      graph.strokeWidth = 2
+
+      this.addChild(graph)
+    })
+  }
+}
